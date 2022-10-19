bad61f52ac4a7b755482e12f9ec2f76bd364b86d
ligia <ligia@DESKTOP-33DDFM8>
2022-10-18 23:31:56 -0300

    exercicio cone

---
+++ cone/src/cone/Cone.java
@@ -0,0 +1,109 @@
+package cone;
+import java.lang.Math; 
+
+
+public class Cone {
+	//atributos
+	private float z;
+	private float r;
+	private int tinta;
+	
+
+	//construtor	
+	public Cone(float r, float z, int tinta) {
+		setZ(r);
+		setR(z);
+		setTinta(tinta);
+	}
+	
+
+	public Cone(float r, int tinta) {
+		setR(r);
+		setTinta(tinta);
+	}
+	
+	public Cone() {
+	}
+	
+	
+	//metodos que criei a parte
+	public double geratriz() {			
+		return  Math.sqrt(Math.pow(r,2) + Math.pow(z,2));
+	}
+	public double Fundo() {
+		return 3.14 * Math.pow(r,2) ;
+	}
+	public double Lateral() {
+		return 3.14 * r * geratriz();
+	}
+	public double TotalTintas() {
+		return 3.14 * r * (geratriz() + r) ;
+	}
+	public double Litros() {
+		return 3.45 * TotalTintas() ;
+	}
+	public double Latas() {
+		return  Math.round(Litros() / 18) ;
+	}
+	public double preco() {
+		if(tinta == 1) {
+			return Latas() * 238.90;
+		} else if (tinta == 2) {
+			return Latas() * 467.98;
+		} else {
+			return Latas() * 758.34;
+		}
+	}
+	
+	
+	//Get e Set
+	public float getZ() {
+		return z;
+	}
+
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+
+
+	public float getR() {
+		return r;
+	}
+
+
+	public void setR(float r) {
+		this.r = r;
+	}
+
+
+	public int getTinta() {
+		return tinta;
+	}
+
+
+	public void setTinta(int tinta) {
+		this.tinta = tinta;
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Cone [z=");
+		builder.append(z);
+		builder.append(", r=");
+		builder.append(r);
+		builder.append(", tinta=");
+		builder.append(tinta);
+		builder.append("]");
+		return builder.toString();
+	}
+
+
+	
+
+	
+
+
+}
---
+++ cone/src/cone/Main.java
@@ -0,0 +1,18 @@
+package cone;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Cone coneUm = new Cone(15.50f, 6.85f, 2);
+		System.out.println("Cone: "+coneUm);
+		System.out.println("\n");
+		
+		
+		
+
+	}
+
+}
